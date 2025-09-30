557edd3660d749965d5de5b0ffb2267e28e8856e
ArmanShaikh05 <arman.shaikh01082003@gmail.com>
2025-09-30 21:09:42 +0530

    Implemented home page with tradingview widgets

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,43 @@
-import { Button } from "@/components/ui/button";
+import TradingViewWidget from "@/components/TradingViewWidget";
+import { HEATMAP_WIDGET_CONFIG, MARKET_DATA_WIDGET_CONFIG, TOP_STORIES_WIDGET_CONFIG } from "@/lib/constants";
 
 const Home = () => {
   return (
     <div className="flex min-h-screen home-wrapper">
-      <Button>Click Me</Button>
+      <section className="grid w-full home-section">
+        <div className="md:col-span-1 xl:col-span-1">
+          <TradingViewWidget
+            title="Maket Overview"
+            scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-market-overview.js"
+            config={MARKET_DATA_WIDGET_CONFIG}
+            className="custom-chart"
+          />
+        </div>
+
+        <div className="md:col-span-1 xl:col-span-2 ">
+          <TradingViewWidget
+            title="Stock Heatmap"
+            scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-stock-heatmap.js"
+            config={HEATMAP_WIDGET_CONFIG}
+          />
+        </div>
+      </section>
+      <section className="grid w-full home-section">
+        <div className="h-full md:col-span-1 xl:col-span-1">
+          <TradingViewWidget
+            scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-timeline.js"
+            config={TOP_STORIES_WIDGET_CONFIG}
+            className="custom-chart"
+          />
+        </div>
+
+        <div className="h-full md:col-span-1 xl:col-span-2 ">
+          <TradingViewWidget
+            scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-market-quotes.js"
+            config={MARKET_DATA_WIDGET_CONFIG}
+          />
+        </div>
+      </section>
     </div>
   );
 };
